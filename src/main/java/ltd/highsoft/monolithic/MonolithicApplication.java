d476be45d991869acc87fe185226443e003e24ef
Van Zhu <van@highsoft.ltd>
2022-04-23 14:02:27 +0800

    feat: initial project structure

---
+++ src/main/java/ltd/highsoft/monolithic/MonolithicApplication.java
@@ -0,0 +1,13 @@
+package ltd.highsoft.monolithic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonolithicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonolithicApplication.class, args);
+    }
+
+}
