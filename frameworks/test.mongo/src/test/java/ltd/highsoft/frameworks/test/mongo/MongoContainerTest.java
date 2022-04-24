9ce401b5c27b1bd3ef412c7993bdc5ddf9b256fc
Van Zhu <van@highsoft.ltd>
2022-04-24 13:52:13 +0800

    feat: test logging

---
+++ frameworks/test.mongo/src/test/java/ltd/highsoft/frameworks/test/mongo/MongoContainerTest.java
@@ -0,0 +1,43 @@
+package ltd.highsoft.frameworks.test.mongo;
+
+import com.mongodb.client.*;
+import ltd.highsoft.frameworks.test.container.WithTestContainers;
+import org.bson.Document;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@WithTestContainers(containers = MongoContainer.class)
+class MongoContainerTest {
+
+    private MongoClient mongoClient;
+    private MongoCollection<Document> collection;
+
+    @BeforeEach
+    void setUp() {
+        mongoClient = MongoClients.create(System.getProperty("spring.data.mongodb.uri"));
+        MongoDatabase database = mongoClient.getDatabase("test");
+        collection = database.getCollection("test");
+    }
+
+    @Test
+    void should_launch_mongodb_test_container() {
+        collection.insertOne(new Document(Map.of("_id", "test.mongo", "name", "John")));
+        MongoCursor<Document> cursor = collection.find().cursor();
+        assertThat(cursor).isNotNull();
+        assertThat(cursor.hasNext()).isTrue();
+        assertThat(cursor.next()).isEqualTo(new Document(Map.of("_id", "test.mongo", "name", "John")));
+        assertThat(cursor.hasNext()).isFalse();
+    }
+
+    @AfterEach
+    void tearDown() {
+        collection.drop();
+        mongoClient.close();
+    }
+
+}
