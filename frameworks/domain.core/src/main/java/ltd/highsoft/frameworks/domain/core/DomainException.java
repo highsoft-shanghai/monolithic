d6b39eb91e778efd451868ddcaf06a919bc432d0
Van Zhu <van@highsoft.ltd>
2022-04-24 20:52:47 +0800

    feat: DomainExceptionTest.should_be_able_to_format_as_message

--- frameworks/domain.core/src/main/java/ltd/highsoft/frameworks/domain/core/DomainException.java
+++ frameworks/domain.core/src/main/java/ltd/highsoft/frameworks/domain/core/DomainException.java
@@ -1,4 +1,16 @@
 package ltd.highsoft.frameworks.domain.core;
 
 public class DomainException extends RuntimeException {
+
+    public DomainException() {
+    }
+
+    public DomainException(String code) {
+        super(code);
+    }
+
+    public String format(MessageResolver resolver) {
+        return resolver.resolve(getMessage());
+    }
+
 }
--- frameworks/domain.core/src/test/java/ltd/highsoft/frameworks/domain/core/DomainExceptionTest.java
+++ frameworks/domain.core/src/test/java/ltd/highsoft/frameworks/domain/core/DomainExceptionTest.java
@@ -1,14 +1,32 @@
 package ltd.highsoft.frameworks.domain.core;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 
 public class DomainExceptionTest {
 
+    private @Mock MessageResolver messageResolver;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
     @Test
-    void should_be_not_checked_exception() {
+    void should_not_be_checked_exception() {
         assertThat(new DomainException()).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    void should_be_able_to_format_as_message() {
+        given(messageResolver.resolve("error.a-test-message-code")).willReturn("Error message from resolver");
+        DomainException exception = new DomainException("error.a-test-message-code");
+        assertThat(exception.format(messageResolver)).isEqualTo("Error message from resolver");
     }
 
 }
