37436ab1b587012abb3b6473233b8bc9be90383d
Van Zhu <van@highsoft.ltd>
2022-04-24 21:57:10 +0800

    refactor

--- frameworks/domain.core/src/main/java/ltd/highsoft/frameworks/domain/core/DomainException.java
+++ frameworks/domain.core/src/main/java/ltd/highsoft/frameworks/domain/core/DomainException.java
@@ -2,10 +2,11 @@ package ltd.highsoft.frameworks.domain.core;
 
 public class DomainException extends RuntimeException {
 
+    private static final Object[] EMPTY_DATA = {};
     private final Object[] data;
 
     public DomainException() {
-        this.data = new Object[]{};
+        this.data = EMPTY_DATA;
     }
 
     public DomainException(String code, Object... data) {
