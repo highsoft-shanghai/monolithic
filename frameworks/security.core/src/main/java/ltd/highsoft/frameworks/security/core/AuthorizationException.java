ec18df448d40947667042af04f0a198bd922021b
Van Zhu <van@highsoft.ltd>
2022-04-28 00:54:28 +0800

    refactor

--- frameworks/security.core/src/main/java/ltd/highsoft/frameworks/security/core/AuthorizationException.java
+++ frameworks/security.core/src/main/java/ltd/highsoft/frameworks/security/core/AuthorizationException.java
@@ -4,21 +4,8 @@ import ltd.highsoft.frameworks.domain.core.DomainException;
 
 public class AuthorizationException extends DomainException {
 
-    private final RequiredAuthorities requiredAuthorities;
-    private final GrantedAuthorities grantedAuthorities;
-
     public AuthorizationException(String code, RequiredAuthorities requiredAuthorities, GrantedAuthorities grantedAuthorities) {
         super(code, requiredAuthorities, grantedAuthorities);
-        this.requiredAuthorities = requiredAuthorities;
-        this.grantedAuthorities = grantedAuthorities;
-    }
-
-    public RequiredAuthorities requiredAuthorities() {
-        return requiredAuthorities;
-    }
-
-    public GrantedAuthorities grantedAuthorities() {
-        return grantedAuthorities;
     }
 
 }
