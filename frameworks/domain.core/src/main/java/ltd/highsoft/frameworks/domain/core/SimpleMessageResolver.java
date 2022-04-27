5c6d5b1732984fb581f7c0965ca1386341803cf2
Van Zhu <van@highsoft.ltd>
2022-04-28 00:39:14 +0800

    refactor

--- frameworks/domain.core/src/main/java/ltd/highsoft/frameworks/domain/core/SimpleMessageResolver.java
+++ frameworks/domain.core/src/main/java/ltd/highsoft/frameworks/domain/core/SimpleMessageResolver.java
@@ -1,12 +1,17 @@
 package ltd.highsoft.frameworks.domain.core;
 
-import org.apache.commons.lang3.StringUtils;
+import java.util.Arrays;
 
 public class SimpleMessageResolver implements MessageResolver {
 
     @Override
     public String resolve(String code, Object... args) {
-        return code + ": [" + StringUtils.joinWith(", ", args) + "]";
+        return code + dataAsString(args);
+    }
+
+    private String dataAsString(Object[] args) {
+        if (args.length < 1) return "";
+        return ": " + Arrays.toString(args);
     }
 
 }
