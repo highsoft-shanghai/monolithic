6e22854fce70ccd984768d4d800e32b0e1c036a2
Van Zhu <van@highsoft.ltd>
2022-05-01 19:49:09 +0800

    refactor

--- frameworks/persistence.spring/src/test/java/ltd/highsoft/frameworks/persistence/spring/SpringPageTest.java
+++ frameworks/persistence.spring/src/test/java/ltd/highsoft/frameworks/persistence/spring/SpringPageTest.java
@@ -1,7 +1,7 @@
 package ltd.highsoft.frameworks.persistence.spring;
 
 import ltd.highsoft.frameworks.domain.core.Page;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.data.domain.*;
 
 import java.util.List;
@@ -10,15 +10,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class SpringPageTest {
 
+    private Page<String> page;
+
+    @BeforeEach
+    void setUp() {
+        page = SpringPage.from(new PageImpl<>(List.of("a", "b"), Pageable.ofSize(5), 30));
+    }
+
     @Test
     void should_be_able_to_carry_content() {
-        Page<String> page = SpringPage.from(new PageImpl<>(List.of("a", "b"), Pageable.ofSize(5), 30));
         assertThat(page.content()).containsExactly("a", "b");
     }
 
     @Test
     void should_be_able_to_carry_number_of_elements() {
-        Page<String> page = SpringPage.from(new PageImpl<>(List.of("a", "b"), Pageable.ofSize(5), 30));
         assertThat(page.numberOfElements()).isEqualTo(2);
     }
 
