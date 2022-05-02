e8ad9f90ff4c27a5a6679f5b02c7ca2b12c1ccdc
Van Zhu <van@highsoft.ltd>
2022-05-03 01:08:26 +0800

    feat: GlobalIdGeneratorExtensionTest.should_be_able_to_reset_security_context

---
+++ frameworks/test.context/src/main/java/ltd/highsoft/frameworks/test/context/GlobalIdGeneratorExtension.java
@@ -0,0 +1,22 @@
+package ltd.highsoft.frameworks.test.context;
+
+import ltd.highsoft.frameworks.domain.core.*;
+import org.junit.jupiter.api.extension.*;
+import org.junit.platform.commons.util.AnnotationUtils;
+
+import java.util.Optional;
+
+public class GlobalIdGeneratorExtension implements BeforeEachCallback, AfterEachCallback {
+
+    @Override
+    public void beforeEach(ExtensionContext context) {
+        Optional<WithFixedGlobalIdGenerator> annotation = AnnotationUtils.findAnnotation(context.getRequiredTestClass(), WithFixedGlobalIdGenerator.class);
+        annotation.ifPresent(x -> GlobalIdGeneratorResetter.reset(new FixedIdGenerator(x.id())));
+    }
+
+    @Override
+    public void afterEach(ExtensionContext context) {
+        GlobalIdGeneratorResetter.reset(new UuidBasedIdGenerator());
+    }
+
+}
---
+++ frameworks/test.context/src/test/java/ltd/highsoft/frameworks/test/context/GlobalIdGeneratorExtensionTest.java
@@ -0,0 +1,17 @@
+package ltd.highsoft.frameworks.test.context;
+
+import ltd.highsoft.frameworks.domain.core.GlobalIdGenerator;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@WithFixedGlobalIdGenerator(id = "a-fixed-id")
+public class GlobalIdGeneratorExtensionTest {
+
+    @Test
+    void should_be_able_to_reset_security_context() {
+        assertThat(GlobalIdGenerator.nextId()).isEqualTo("a-fixed-id");
+        assertThat(GlobalIdGenerator.nextReadableId()).isEqualTo("a-fixed-id");
+    }
+
+}
