453f2b7866e8bfbc080a0ffbed9ad4d88cdd229c
Van Zhu <van@highsoft.ltd>
2022-05-03 14:06:55 +0800

    refactor

--- frameworks/security.core/src/main/java/ltd/highsoft/frameworks/security/core/InvalidSecurityContext.java
+++ frameworks/security.core/src/main/java/ltd/highsoft/frameworks/security/core/InvalidSecurityContext.java
@@ -1,17 +1,17 @@
 package ltd.highsoft.frameworks.security.core;
 
-import ltd.highsoft.frameworks.domain.core.AuthenticationException;
+import ltd.highsoft.frameworks.domain.core.BadAccessTokenException;
 
 public class InvalidSecurityContext implements SecurityContext {
 
     @Override
     public String token() {
-        throw new AuthenticationException("error.bad-token");
+        throw new BadAccessTokenException();
     }
 
     @Override
     public GrantedAuthorities grantedAuthorities() {
-        throw new AuthenticationException("error.bad-token");
+        throw new BadAccessTokenException();
     }
 
     @Override
