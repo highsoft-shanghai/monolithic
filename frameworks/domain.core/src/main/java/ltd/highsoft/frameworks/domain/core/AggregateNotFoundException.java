fe8f616aaa07877c987ac9dab2127a7b968c0339
Van Zhu <van@highsoft.ltd>
2022-05-03 14:23:24 +0800

    fix

--- frameworks/domain.core/src/main/java/ltd/highsoft/frameworks/domain/core/AggregateNotFoundException.java
+++ frameworks/domain.core/src/main/java/ltd/highsoft/frameworks/domain/core/AggregateNotFoundException.java
@@ -2,7 +2,7 @@ package ltd.highsoft.frameworks.domain.core;
 
 public class AggregateNotFoundException extends DomainException {
 
-    public static final String MESSAGE_CODE = "error.object-not-found";
+    public static final String MESSAGE_CODE = "error.aggregate-not-found";
 
     public AggregateNotFoundException(Object... data) {
         super(MESSAGE_CODE, data);
--- frameworks/domain.core/src/test/java/ltd/highsoft/frameworks/domain/core/AggregateNotFoundExceptionTest.java
+++ frameworks/domain.core/src/test/java/ltd/highsoft/frameworks/domain/core/AggregateNotFoundExceptionTest.java
@@ -10,7 +10,7 @@ class AggregateNotFoundExceptionTest {
     void should_be_able_to_carry_rich_error_information() {
         AggregateNotFoundException exception = new AggregateNotFoundException("order", "1");
         assertThat(exception).isInstanceOf(DomainException.class);
-        assertThat(exception).hasMessage("error.object-not-found: [order, 1]");
+        assertThat(exception).hasMessage("error.aggregate-not-found: [order, 1]");
         assertThat(exception.data()).containsExactly("order", "1");
     }
 
@@ -18,7 +18,7 @@ class AggregateNotFoundExceptionTest {
     void should_be_able_to_carry_cause() {
         AggregateNotFoundException exception = new AggregateNotFoundException(new RuntimeException(), "order", "1");
         assertThat(exception).isInstanceOf(DomainException.class);
-        assertThat(exception).hasMessage("error.object-not-found: [order, 1]");
+        assertThat(exception).hasMessage("error.aggregate-not-found: [order, 1]");
         assertThat(exception).hasCauseInstanceOf(RuntimeException.class);
         assertThat(exception.data()).containsExactly("order", "1");
     }
