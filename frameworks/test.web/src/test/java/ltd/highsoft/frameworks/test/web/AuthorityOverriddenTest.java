26b5481991552f698de7f14b8dbeb3b4557611be
Van Zhu <van@highsoft.ltd>
2022-05-07 01:35:04 +0800

    fix: test coverage

--- frameworks/test.web/src/test/java/ltd/highsoft/frameworks/test/web/AuthorityOverriddenTest.java
+++ frameworks/test.web/src/test/java/ltd/highsoft/frameworks/test/web/AuthorityOverriddenTest.java
@@ -1,6 +1,6 @@
 package ltd.highsoft.frameworks.test.web;
 
-import ltd.highsoft.frameworks.security.core.GrantedAuthorities;
+import ltd.highsoft.frameworks.security.core.*;
 import org.junit.jupiter.api.Test;
 
 import javax.annotation.Resource;
@@ -14,9 +14,11 @@ public class AuthorityOverriddenTest extends IntegrationTest {
 
     @Test
     void should_be_able_to_override_authorities_for_current_user() {
-        var context = contextProviderHolder.contextProvider().get(GlobalTestContext.DEFAULT_TESTER_ACCESS_TOKEN);
+        var context = contextProviderHolder.contextProvider().get(GlobalTestContext.DEFAULT_TESTER_ACCESS_TOKEN).map(Context::securityContext);
         assertThat(GlobalTestContext.accessToken()).hasValue(GlobalTestContext.DEFAULT_TESTER_ACCESS_TOKEN);
-        assertThat(context.map(x -> x.securityContext().grantedAuthorities())).hasValue(GrantedAuthorities.of("f3", "f4"));
+        assertThat(context.map(SecurityContext::grantedAuthorities)).hasValue(GrantedAuthorities.of("f3", "f4"));
+        assertThat(context.map(SecurityContext::token)).hasValue(GlobalTestContext.DEFAULT_TESTER_ACCESS_TOKEN);
+        assertThat(context.map(SecurityContext::valid)).hasValue(true);
     }
 
 }
