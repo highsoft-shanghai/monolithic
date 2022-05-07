3fa1bb4475a96f5cabfa508a68b822346648f7b2
Van Zhu <van@highsoft.ltd>
2022-05-08 02:30:20 +0800

    refactor

--- src/test/java/ltd/highsoft/monolithic/usecases/iam/GetAccessTokenContentUseCaseTest.java
+++ src/test/java/ltd/highsoft/monolithic/usecases/iam/GetAccessTokenContentUseCaseTest.java
@@ -1,31 +1,32 @@
 package ltd.highsoft.monolithic.usecases.iam;
 
 import ltd.highsoft.frameworks.security.core.Authorities;
-import ltd.highsoft.frameworks.test.web.WithGrantedAuthorities;
+import ltd.highsoft.frameworks.test.web.*;
 import ltd.highsoft.monolithic.IntegrationTest;
 import org.junit.jupiter.api.Test;
 
 import static ltd.highsoft.frameworks.test.web.Documentation.doc;
 import static org.hamcrest.Matchers.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 
 public class GetAccessTokenContentUseCaseTest extends IntegrationTest {
 
     @Test
     @WithGrantedAuthorities(Authorities.AUTHENTICATED)
     void should_be_able_to_get_content_of_current_access_token() {
-        var response = get("/access-tokens/current", doc("access-tokens.current.get",
-//            pathParameters(
-//            ),
+        var response = get("/access-tokens/current", document());
+        response.statusCode(is(200));
+        response.body("accessToken", is("tester-access-token"));
+        response.body("authorities", hasItems("feature-1", "feature-2"));
+    }
+
+    private Documentation document() {
+        return doc("access-tokens.current.get",
             responseFields(
                 fieldWithPath("accessToken").description("访问令牌标识"),
                 fieldWithPath("authorities").description("授予的权限集合")
             )
-        ));
-        response.statusCode(is(200));
-        response.body("accessToken", is("tester-access-token"));
-        response.body("authorities", hasItems("feature-1", "feature-2"));
+        );
     }
 
 }
