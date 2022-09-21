a3747e9e1993d2bb9b51cf10e5ec4300f3142262
Neil <wangweili457@gmail.com>
2022-09-22 00:50:04 +0800

    REFACTOR

--- frameworks/domain.core/src/main/java/ltd/highsoft/frameworks/domain/core/archtype/One.java
+++ frameworks/domain.core/src/main/java/ltd/highsoft/frameworks/domain/core/archtype/One.java
@@ -2,6 +2,6 @@ package ltd.highsoft.frameworks.domain.core.archtype;
 
 public interface One<Aggregate extends ltd.highsoft.frameworks.domain.core.archtype.Aggregate> {
 
-    ltd.highsoft.frameworks.domain.core.archtype.Aggregate get();
+    Aggregate get();
 
 }
